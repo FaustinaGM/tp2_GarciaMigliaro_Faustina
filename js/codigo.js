e5a84d77a850e2d992ff18a871e160012fb03b99
FaustinaGM <136551531+FaustinaGM@users.noreply.github.com>
2023-06-14 01:48:52 -0300

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,11 @@
+document.getElementById('menu').addEventListener('click', function()
+{
+document.getElementById('navega').classList.toggle('mostrar');
+});
+
+function reproducir() {
+    document.getElementById('miVideo').play();
+}
+function pausar() {
+    document.getElementById('miVideo').pause();
+}
---
+++ js/codigoLightbox.js
@@ -0,0 +1,8 @@
+
+    lightbox.option({
+      'wrapAround': true,
+      'albumLabel':"Imagen %1 de %2",
+    })
+
+
+
